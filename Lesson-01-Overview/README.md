c31d76dedd9361f1e447cdcef17d140cc8f7675b
hoangduc011097 <hhuuduc97@gmail.com>
2023-07-20 15:39:38 +0700

    lesson 1

---
+++ Lesson-01-Overview/README.md
@@ -0,0 +1,8 @@
+// // Mern stack (JS)
+//     M Monggodb
+//     E Express
+//     R ReactJS
+//     N NodeJS
+
+
+common JS != ES6 module
---
+++ Lesson-01-Overview/index.js
@@ -0,0 +1,40 @@
+
+// console.log('hello world nodeJS')
+// const Sum = (a,b ) => {
+//     return a + b
+// }
+
+const { sum } = require('./math');
+const fs = require("fs");
+const express =  require("express");
+
+
+const a =4 ;
+const b = 5;
+const result = sum(a,b);
+
+// console.log(`Sum between ${a} +  ${b} = ${result}`);
+
+
+// fs.readFile("./text.txt", "utf-8",(err, data)=>{
+
+//     if(err){
+//         console.error('Some thing went wrong');
+
+//     }
+//         console.log('Data:', data)
+    
+
+// })
+
+const app =express();
+const port = 3000;
+
+app.get("/" , (req,res)=>{
+    res.send("this is first web service");
+
+});
+
+app.listen(port, ()=>{
+    console.log(`My server is running at PORT = ${port}`)
+})
